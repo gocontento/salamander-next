9f2b27da10b89200c4785df096a066a405d0aa82
Josh Angell <josh@angell.io>
2023-09-28 15:38:28 +0100

    box component

--- app/components/block-matcher.tsx
+++ app/components/block-matcher.tsx
@@ -1,6 +1,7 @@
 import { BlockApiData } from "@gocontento/client/lib/api-types";
 import Hero from "@/app/components/blocks/hero";
 import ImageAndText from "@/app/components/blocks/image-and-text";
+import Box from "@/app/components/blocks/box";
 
 export default function BlockMatcher({block}: { block: BlockApiData }) {
     switch (block.content_type.handle) {
@@ -12,6 +13,11 @@ export default function BlockMatcher({block}: { block: BlockApiData }) {
         case "image_and_text":
             return (
                 <ImageAndText block={block} />
+            );
+
+        case "box_block":
+            return (
+                <Box block={block} />
             );
 
         default:
