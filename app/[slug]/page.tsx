1ae88b33b41a70c74a03ebab0dad34b1c552c803
Josh Angell <josh@angell.io>
2023-09-29 15:09:12 +0100

    prose text block, and root slug page sorted to be generic

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -19,24 +19,31 @@ export default async function GeneralPage({ params }: Props) {
 
     const response = await client.getContent({
         params: {
-            content_type: "general_page",
+            "content_type[0]": "general_page",
+            "content_type[1]": "info_page",
             slug: params.slug,
             limit: "1"
         }
     });
 
-    const homePageContent = response.content[0] as ContentApiData;
+    const pageContent = response.content[0] as ContentApiData;
 
     return (
-        <div className="pb-12 md:pb-32">
-            <PreviewBridge draftMode={isEnabled} />
+        <article className={`pb-12 md:pb-32 ${pageContent.content_type.handle === "info_page" ? 'max-w-prose mx-auto' : ''}`}>
+            <PreviewBridge draftMode={isEnabled}/>
 
-            {/* TODO: handle info page here as well, probably break out the following stuff to a component or do a simple if statement */}
-            {homePageContent.fields.content.blocks.map((block: BlockApiData) => {
+            {pageContent.content_type.handle === "info_page" &&
+                <header className="md:pt-16 md:text-center pb-7 border-b-2 border-charcoal">
+                    <h1 className="text-xl font-bold mb-5">{pageContent.fields.heading.text}</h1>
+                    <p>{pageContent.fields.intro_text.text}</p>
+                </header>
+            }
+
+            {pageContent.fields.content.blocks.map((block: BlockApiData) => {
                 return (
-                    <BlockMatcher key={`${block.name}-${block.sort}`} block={block} />
+                    <BlockMatcher key={`${block.name}-${block.sort}`} block={block}/>
                 )
             })}
-        </div>
+        </article>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,9 +33,40 @@ const config: Config = {
         xxl: '3.8rem'
 
       },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            ul: {
+              'list-style-type': 'square'
+            }
+          }
+        },
+        black: {
+          css: {
+            '--tw-prose-body': theme('colors.black'),
+            '--tw-prose-headings': theme('colors.black'),
+            '--tw-prose-lead': theme('colors.black'),
+            '--tw-prose-links': theme('colors.black'),
+            '--tw-prose-bold': theme('colors.black'),
+            '--tw-prose-counters': theme('colors.black'),
+            '--tw-prose-bullets': theme('colors.black'),
+            '--tw-prose-hr': theme('colors.black'),
+            '--tw-prose-quotes': theme('colors.black'),
+            '--tw-prose-quote-borders': theme('colors.black'),
+            '--tw-prose-captions': theme('colors.black'),
+            '--tw-prose-code': theme('colors.black'),
+            '--tw-prose-pre-code': theme('colors.white'),
+            '--tw-prose-pre-bg': theme('colors.black'),
+            '--tw-prose-th-borders': theme('colors.black'),
+            '--tw-prose-td-borders': theme('colors.black'),
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
 export default config
 
