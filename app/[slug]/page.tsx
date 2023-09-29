0838673234cf1b2d3ca3da9cf30b8745518bb8c2
Josh Angell <josh@angell.io>
2023-09-29 11:49:08 +0100

    slug page

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,40 @@
+import { draftMode } from 'next/headers';
+import { createClient } from "@/lib/contento";
+import { PreviewBridge } from "@gocontento/next";
+import {BlockApiData, ContentApiData} from "@gocontento/client/lib/api-types";
+import BlockMatcher from "@/app/components/block-matcher";
+
+const client = createClient();
+
+type Props = {
+    params: {
+        slug: string;
+    };
+};
+
+export default async function GeneralPage({ params }: Props) {
+
+    const { isEnabled } = draftMode();
+
+    const response = await client.getContent({
+        params: {
+            content_type: "general_page",
+            slug: params.slug,
+            limit: "1"
+        }
+    });
+
+    const homePageContent = response.content[0] as ContentApiData;
+
+    return (
+        <div className="pb-12 md:pb-32">
+            <PreviewBridge draftMode={isEnabled} />
+
+            {homePageContent.fields.content.blocks.map((block: BlockApiData) => {
+                return (
+                    <BlockMatcher key={`${block.name}-${block.sort}`} block={block} />
+                )
+            })}
+        </div>
+    )
+}
