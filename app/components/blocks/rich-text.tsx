db0646de514e08069dba259a6122eaa92e6a9425
Josh Angell <josh@angell.io>
2023-10-03 23:47:28 +0100

    center the text block

--- app/components/blocks/rich-text.tsx
+++ app/components/blocks/rich-text.tsx
@@ -3,7 +3,7 @@ import {BlockApiData} from "@gocontento/client/lib/api-types";
 export default function RichText({ block }: { block: BlockApiData }) {
     return (
         <div
-            className="mt-5 prose prose-black"
+            className="mt-5 prose prose-black mx-auto"
             dangerouslySetInnerHTML={{
                 __html: block.fields.rich_text_field.text,
             }}
