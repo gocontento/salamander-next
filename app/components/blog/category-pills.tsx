ad690c9e79399b0a10cb2343550a091210cf9617
Josh Angell <josh@angell.io>
2023-10-03 23:22:02 +0100

    author block

--- app/components/blog/category-pills.tsx
+++ app/components/blog/category-pills.tsx
@@ -20,7 +20,7 @@ const links = [
 export default function CategoryPills() {
     const pathname = usePathname();
     return (
-        <div className="py-7 mx-auto inline-flex flex-wrap items-center width-100 md:justify-between gap-x-2 gap-y-4 md:gap-5">
+        <div className="py-7 mx-auto inline-flex flex-wrap items-center md:justify-between gap-x-2 gap-y-4 md:gap-5">
             {links.map((link) => {
                 return (
                     <Link key={link.href} href={link.href} className={`text-[12px] px-5 py-2 text-white rounded-full ${pathname === link.href ? 'bg-pink' : 'bg-charcoal hover:bg-pink'} hover:text-white`}>
